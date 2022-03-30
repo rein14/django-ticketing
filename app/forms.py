a7217d9ea0277aed115ca004acee27ed3af7fcae
rein14 <reinrich14@gmail.com>
2022-03-30 13:32:09 +0000

    add to user selction field select2

--- app/forms.py
+++ app/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from .models import Ticket, Comment, File
 from bootstrap_datepicker_plus import DatePickerInput
 from cms.forms import BootstrapHelperForm
-# from django_select2 import forms as s2forms
+from django_select2 import forms as s2forms
 
 
-# class TicketWidget(s2forms.ModelSelect2MultipleWidget):
-#     search_fields = [
-#          "username__icontains",
-#          "email__icontains",
-#     ]
+class TicketWidget(s2forms.ModelSelect2MultipleWidget):
+    search_fields = [
+         "last_name__icontains",
+        "first_name__icontains",
+    ]
 
 class TicketForm(BootstrapHelperForm, forms.ModelForm):
     class Meta:
@@ -18,7 +18,7 @@ class TicketForm(BootstrapHelperForm, forms.ModelForm):
                   'assigned_to', 'sent_by')
         widgets = {
             'date_sent': DatePickerInput(format='%Y-%m-%d'),
-            #'assigned_to': TicketWidget,
+            'assigned_to' : TicketWidget,
         }
 
 
@@ -29,7 +29,7 @@ class TicketUpdateForm(BootstrapHelperForm, forms.ModelForm):
                   'assigned_to', 'waiting_for', 'sent_by')
         widgets = {
             'date_sent': DatePickerInput(format='%Y-%m-%d'),
-            #'assigned_to' : TicketWidget,
+            'assigned_to' : TicketWidget,
         }
 
 
