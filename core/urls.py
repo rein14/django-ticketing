467b7eaf064133ae6ea6f4d833109b5deabf8eb3
rein14 <reinrich14@gmail.com>
2022-05-02 05:18:33 +0000

    server

--- core/urls.py
+++ core/urls.py
@@ -43,5 +43,5 @@ if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.views import serve
     from django.views.decorators.cache import never_cache
-    urlpatterns.append(path('static/<path:path>', never_cache(serve)))
+    urlpatterns.append(path('static/<path:path>', serve))
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
