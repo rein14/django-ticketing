85c0ddd5a15381d18f6441af5342eacf4f8babce
rein14 <reinrich14@gmail.com>
2022-05-04 18:26:03 +0000

    changes

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,6 +1,6 @@
 from .models import Folder, Comment
 
 def nav_cats(request):
-    objects = Folder.objects.all()[:10]
+    objects = Folder.objects.all()[:5]
     #comments = Comment.objects.all()
     return {'nav_folder': objects}
